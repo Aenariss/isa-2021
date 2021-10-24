7b88c793b4acd0f3579bc081feedabb441ae8d11
isa <aenariss@gmail.com>
2021-10-24 22:31:19 +0200

    add ipv6 support

--- my_client.cpp
+++ my_client.cpp
@@ -15,6 +15,9 @@
 #include <sstream>
 #include "base64.h"
 
+#define print(x) (std::cout << x << '\n');  // debug makro
+
+/* Funkce na vypsani napovedy podle referencniho klienta */
 void print_help() {
     std::cout << "usage: client [ <option> ... ] <command> [<args>] ...\n\n";
     std::cout << "<option> is one of\n\n";
@@ -31,8 +34,10 @@ void print_help() {
     std::cout << "   list\n   send <recipient> <subject> <body>\n   fetch <id>\n   logout\n";
 }
 
+/* Funkce na zjisteni, jestli je zadany string cislo */
 bool is_number(std::string string) {
     size_t limit = string.length();
+    // Jestlize nejaka hodnota ve stringu neni cislo, tak se o cislo nejedna
     for (size_t i = 0; i < limit; i++) {
         if (!(isdigit(string[i])))
             return false;
@@ -54,18 +59,21 @@ std::tuple<char*, char*> get_user_data(int index, char **args) {
     return std::make_tuple(args[index+1], args[index+2]);
 }
 
+/* Pomocna struktura na zpracovavani argumentu - obsahuje vsechny mozne argumenty */
 struct Parsed_args {
     char *user_name, *user_password, *recipient, *subject, *body, *id;
     bool reg, list, send, fetch, logout, login;
     char *addr, *port;
 };
 
+/* Funkce na vypis adresy a portu, pouzivana v pripade chyby -- ukonci program s chybou */
 void print_error(Parsed_args args) {
     std::cout << "  hostname: " << args.addr << '\n';
     std::cout << "  port number: " << args.port << '\n';
     exit(1);
 }
 
+/* Funkce pro zpracovani argumentu */
 Parsed_args parse_args(int argc, char *argv[]) {
     char *user_name, *user_password, *recipient, *subject, *body, *id;
     user_name = user_password = recipient = subject = body = id = nullptr;
@@ -74,58 +82,62 @@ Parsed_args parse_args(int argc, char *argv[]) {
     std::string addr, port;
 
     if (argc == 1) {
-        std::cout << "client: expects <command> [<args>] ... on the command line, given 0 arguments\n";
+        std::cerr << "client: expects <command> [<args>] ... on the command line, given 0 arguments\n";
         exit(1);
     }
 
     std::regex switch_reg("^-");
+    std::regex check_reg("^--");
     std::smatch result_match;
     bool found = false;
 
     for (int i = 1; i < argc; i++) {
 
         std::string str(argv[i]);
-        if (std::regex_search(str, result_match, switch_reg)) {
-            size_t length = strlen(argv[i]);
-            for (size_t k = 1; k < length; k++) {
+        if (!std::regex_search(str, result_match, check_reg)) {
+            if (std::regex_search(str, result_match, switch_reg)) {
+                size_t length = strlen(argv[i]);
+                for (size_t k = 1; k < length; k++) {
 
-                if (argv[i][k] == 'h') {
-                    print_help();
-                    exit(0);
-                }
-                else if (argv[i][k] == 'p') {
-                    if (i+1 > argc-1) {
-                        std::cout << "client: the \"-p\" option needs 1 argument, but 0 provided\n";
-                        exit(1);
+                    if (argv[i][k] == 'h') {
+                        print_help();
+                        exit(0);
                     }
-                    else {
-                        port = argv[i+1];
-                        // Port a jeho kontrola maji prednost pred vsim ostatnim, podle ref. reseni
-                        if(!(is_number(port))) {
-                            std::cout << "Port number is not a string\n";
+                    else if (argv[i][k] == 'p') {
+                        if (i+1 > argc-1) {
+                            std::cerr << "client: the \"-p\" option needs 1 argument, but 0 provided\n";
                             exit(1);
                         }
+                        else {
+                            port = argv[i+1];
+                            found = true;
+                            // Port a jeho kontrola maji prednost pred vsim ostatnim, podle ref. reseni
+                            if(!(is_number(port))) {
+                                std::cerr << "Port number is not a string\n";
+                                exit(1);
+                            }
+                        }
                     }
-                }
-                else if (argv[i][k] == 'a') {
-                    if (i+1 > argc-1) {
-                        std::cout << "client: the \"-a\" option needs 1 argument, but 0 provided\n";
-                        exit(1);
+                    else if (argv[i][k] == 'a') {
+                        if (i+1 > argc-1) {
+                            std::cerr << "client: the \"-a\" option needs 1 argument, but 0 provided\n";
+                            exit(1);
+                        }
+                        else {
+                            addr = argv[i+1];
+                            found = true;
+                        }
                     }
                     else {
-                        addr = argv[i+1];
+                        std::cerr << "client: unknown switch: -" << argv[i][k] << '\n';
+                        exit(1); 
                     }
-                }
-                else {
-                    std::cout << "client: unknown switch: -" << argv[i][k] << '\n';
-                    exit(1); 
                 }
             }
         }
         // Jestlize jsem nasel port nebo adresu uz pri multi-prepinaci, musim se posunout o 2
         // 1 za zpracovany multiprepinac, 1 za zpracovany argument
-        if ((port.empty() || addr.empty()) && !(found)) {
-            found = true;
+        if ((port.empty() || addr.empty()) && (found)) {
             i+=2;
         }
             
@@ -134,9 +146,10 @@ Parsed_args parse_args(int argc, char *argv[]) {
         if (addr.empty())
             addr = "localhost";
 
+
         if (!strcmp(argv[i], "register")) {
             if (i+2 > argc-1 || i+2 < argc-1 || port.empty() || addr.empty()) {
-                std::cout << "register <username> <password>\n";
+                std::cerr << "register <username> <password>\n";
                 exit(1);
             }
             else {
@@ -148,7 +161,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
 
         else if (!strcmp(argv[i], "login")) {
             if (i+2 > argc-1 || i+2 < argc-1 || port.empty() || addr.empty()) {
-                std::cout << "login <username> <password>\n";
+                std::cerr << "login <username> <password>\n";
                 exit(1);
             }
             else {
@@ -159,7 +172,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
         }
         else if (!strcmp(argv[i], "list")) {
             if (i < argc-1 || port.empty() || addr.empty()) {
-                std::cout << "list\n";
+                std::cerr << "list\n";
                 exit(1);
             }
             list = true;
@@ -168,7 +181,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
         
         else if (!strcmp(argv[i], "send")) {
             if (i+3 > argc-1 || port.empty() || addr.empty()) {
-                std::cout << "send <recipient> <subject> <body>\n";
+                std::cerr << "send <recipient> <subject> <body>\n";
                 exit(1);
             }
             else {
@@ -183,7 +196,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
 
         else if (!strcmp(argv[i], "fetch")) {
             if (i+1 > argc-1 || port.empty() || addr.empty()) {
-                std::cout << "fetch <id>\n";
+                std::cerr << "fetch <id>\n";
                 exit(1);
             }
             else {
@@ -195,7 +208,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
 
         else if (!strcmp(argv[i], "logout")) {
             if (i < argc-1 || port.empty() || addr.empty()) {
-                std::cout << "logout\n";
+                std::cerr << "logout\n";
                 exit(1);
             }
             else {
@@ -204,9 +217,9 @@ Parsed_args parse_args(int argc, char *argv[]) {
             }
         }
 
-        else if (!strcmp(argv[i], "-a") || !strcmp(argv[i], "--addr")) {
+        else if (!strcmp(argv[i], "-a") || !strcmp(argv[i], "--address")) {
             if (i+1 > argc-1) {
-                std::cout << "client: the \"-a\" option needs 1 argument, but 0 provided\n";
+                std::cerr << "client: the \"-a\" option needs 1 argument, but 0 provided\n";
                 exit(1);
             }
             else {
@@ -217,7 +230,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
         
         else if (!strcmp(argv[i], "-p") || !strcmp(argv[i], "--port")) {
             if (i+1 > argc-1) {
-                std::cout << "client: the \"-p\" option needs 1 argument, but 0 provided\n";
+                std::cerr << "client: the \"-p\" option needs 1 argument, but 0 provided\n";
                 exit(1);
             }
             else {
@@ -232,14 +245,14 @@ Parsed_args parse_args(int argc, char *argv[]) {
         }
 
         else {
-            std::cout << "unknown command\n";
-            std::cout << argv[i] << i << '\n';
+            std::cerr << "unknown command\n";
+            std::cerr << argv[i] << i << '\n';
             exit(1);
         }
     }
 
     if (!reg && !list && !send && !fetch && !logout && !login) {
-        std::cout << "client: expects <command> [<args>] ... on the command line, given 0 arguments\n";
+        std::cerr << "client: expects <command> [<args>] ... on the command line, given 0 arguments\n";
         exit(1);
     }
 
@@ -248,6 +261,7 @@ Parsed_args parse_args(int argc, char *argv[]) {
 }
 
 // https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+/* Funkce pro nacteni user tokenu ze souboru */
 std::string read_user_token() {
 
     std::ifstream f("login-token");
@@ -262,6 +276,7 @@ std::string read_user_token() {
     return token.str();
 }
 
+/* Vytvoreni zpravy pro prikazy "login" a "register" */
 std::string create_login_register_message(Parsed_args args, std::string msg) {
 
     if (args.login) {
@@ -270,41 +285,52 @@ std::string create_login_register_message(Parsed_args args, std::string msg) {
     else if (args.reg) {
         msg += "register \"";
     }
+
+    // U prikazu login a register se pouziva base64 kodovani pro heslo 
     std::vector<unsigned char> password(args.user_password, args.user_password + strlen(args.user_password));
     std::string encoded_password = base64_encode(&password[0], password.size());
     msg = msg + args.user_name + "\" \"" +  encoded_password + "\"";
     return msg;
 }
 
+/* Vytvoreni zpravy pro prikaz "send" */
 std::string create_send_message(Parsed_args args, std::string msg) {
     std::string user_token = read_user_token();
     msg = msg + "send " + user_token + " \"" + args.recipient + "\" \"" + args.subject + "\" \"" + args.body + "\"";
     return msg;
 }
 
+/* Vytvoreni zpravy pro prikaz "fetch" */
 std::string create_fetch_message(Parsed_args args, std::string msg) {
     std::string user_token = read_user_token();
     msg = msg +  "fetch " + user_token + " " + args.id;
     return msg;
 }
 
+/* Vytvoreni zpravy pro prikaz "list" */
 std::string create_list_message(std::string msg) {
     std::string user_token = read_user_token();
     msg = msg + "list " + user_token;
     return msg;
 }
 
+/* Vytvoreni zpravy pro prikaz "logout" */
 std::string create_logout_message(std::string msg) {
     std::string user_token = read_user_token();
     msg = msg + "logout " + user_token;
     return msg;
 }
 
+/* Funkce pro vytvoreni tela TCP paketu, kterym je zprava,
+ * ktera je na server poslana 
+*/
 std::string create_tcp_body(Parsed_args args) {
     std::string msg;
+    // Zprava vzdy zacina zavorkou
     msg += '(';
 
     // Tvorba tela TCP paketu, ktery budu posilat
+    // Podle zadanych argumentu rozlisuju, jaka zprava to bude
     if (args.list) {
         msg = create_list_message(msg);
     }
@@ -325,6 +351,7 @@ std::string create_tcp_body(Parsed_args args) {
         msg = create_logout_message(msg);
     }
 
+    // Zprava vzdy zavorkou i konci
     msg += ')';
 
     return msg;
@@ -458,85 +485,86 @@ void print_response(Parsed_args args, char* buffer) {
         */
 
         std::string body = get_nth_part_of_response(buffer_string, 1);
-        std::cout << "ERROR: " << body << '\n';
+        std::cerr << "ERROR: " << body << '\n';
     }
     
     else {
-        std::cout << "Unknown internal error (received wrong packet)\n";
+        std::cerr << "Unknown internal error (received wrong packet)\n";
         exit(1);
     }
 }
 
+/* Funkce na kontrolu argumentu, ktera vlastne akorat zkontroluje, jestli je port cislo */
 void check_args(Parsed_args args) {
     if (!(is_number(args.port))) {
-        std::cout << "Port number is not a string\n";
+        std::cerr << "Port number is not a string\n";
         exit(1);
     }
 }
 
-// https://www.geeksforgeeks.org/socket-programming-cc/
+// https://man7.org/linux/man-pages/man3/getaddrinfo.3.html
+/*
+ * Hlavni ridici funkce, ktera posila zpravy na server a zpracovava odpovedi
+*/
 void send_and_receive(Parsed_args args) {
-
-
-    struct sockaddr_in serv_addr;
+    
+    struct addrinfo hints;
+    struct addrinfo *result, *tmp;
+    int sock, s;
     char buffer[2048];
 
-    //printf("%ld %s\n", strlen(msg), msg);
+    hints.ai_family = AF_UNSPEC;     // Pro IPv4 i IPv6
+    hints.ai_socktype = SOCK_STREAM; // SOCK_STREAM pro TCP
+    hints.ai_flags = 0;              // Nastavuju v podstate jenom kvuli getaddrinfo
+    hints.ai_protocol = 0;           // Protokol pro socktype
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        std::cout << "Couldnt open a connection\n";
+    // Konverze adresi (i pripadneho hostname) na neco, s cim muzu pracovat
+    s = getaddrinfo(args.addr, args.port, &hints, &result);
+    if (s != 0) {
+        std::cerr << "Error with address resolution \n";
         exit(1);
     }
 
-    // Nastav protokol a port
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(atoi(args.port));
+    /* getaddrinfo vraci seznam adresovych struktur, takze musim zjistit,
+     * na jakou z nich se mi povede pripojit 
+    */
+    for (tmp = result; tmp != NULL; tmp = tmp->ai_next) {
+        sock = socket(tmp->ai_family, tmp->ai_socktype, tmp->ai_protocol);
+        // Kdyz byla tahle adresa neuspech (Nepovedlo se vytvorit socket), pokracuju dal
+        if (sock == -1)
+            continue;
 
-
-    // IP muze byt i hostname, takze prelozit
-    int res = inet_pton(AF_INET, args.addr, &serv_addr.sin_addr);
-    if (res <= 0) {
-        // Prvni pokus byl error, tzn. IPv4 to neni, zkusime hostname
-        auto he = gethostbyname (args.addr);
-        // Kdyz to neni ani hostname, tak nic
-        if (he == NULL) {
-            std::cout << "tcp-connect: host not found\n";
-            exit(1);
-        }
-        else {
-            // Ziskanou adresu je potreba zkonvertovat z bytu na char*
-            int res = inet_pton(AF_INET, inet_ntoa(*(struct in_addr*)he->h_addr), &serv_addr.sin_addr);
-            if (res <= 0) {
-                // Tohle by teoreticky nemelo nastat
-                std::cout << "Couldnt translate hostname\n";
-                exit(1);
-            }
-        }
+        // Pokud se povedlo vytvorit socket a pripojit se na nej, muzu koncit prohledavani
+        if (connect(sock, tmp->ai_addr, tmp->ai_addrlen) != -1)
+            break;
     }
 
-    int connection = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    if (connection == -1) {
-        std::cout << "Couldnt open a connection\n";
+    // Pokud se na zadnou adresu pripojit nepovedlo, je neco spatne a musim skoncit
+    if (tmp == NULL) {
+        std::cerr << "tcp connection failed:" << '\n';
         print_error(args);
     }
-
+    
+    // Pokud jsem se dostal az sem, tak se vsechno povedlo a ja muzu poslat paket se zpravou
     check_args(args);
     std::string msg = create_tcp_body(args);
     send(sock, msg.c_str(), msg.length(), 0);
 
     int valread = read(sock, buffer, 2048);
+    // Jestlize se nepovedlo zpracovat odpoved, doslo k nejake chybe po ceste paketu
     if (valread == -1) {
-        std::cout << "Unknown error during packet reading\n";
+        std::cerr << "Unknown error during packet reading\n";
         exit(1);
     }
 
+    // Jestlize k chybe nedoslo, muzu uvolnit pamet, vypsat odpoved serveru a skoncit
+    close(sock);
+    freeaddrinfo(result);
 
     print_response(args, buffer);
 }
 
 int main(int argc, char **argv) {
-
     Parsed_args args = parse_args(argc, argv);
     send_and_receive(args);
     return 0;
